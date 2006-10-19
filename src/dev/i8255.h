773466348ffb3863c8bbdccd16b08bda741c45ea
Olivier Poncet <ponceto@free.fr>
2006-10-19 07:19:40 +0000

    upd765 & i8255 added in libdev

---
+++ src/dev/i8255.h
@@ -0,0 +1,48 @@
+/*
+ * i8255.h - Copyright (c) 2001, 2006 Olivier Poncet
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __GDEV_I8255_H__
+#define __GDEV_I8255_H__
+
+#include <dev/device.h>
+
+G_BEGIN_DECLS
+
+#define GDEV_TYPE_I8255            (gdev_i8255_get_type())
+#define GDEV_I8255(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), GDEV_TYPE_I8255, GdevI8255))
+#define GDEV_I8255_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), GDEV_TYPE_I8255, GdevI8255Class))
+#define GDEV_IS_I8255(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), GDEV_TYPE_I8255))
+#define GDEV_IS_I8255_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), GDEV_TYPE_I8255))
+#define GDEV_I8255_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), GDEV_TYPE_I8255, GdevI8255Class))
+
+typedef struct _GdevI8255      GdevI8255;
+typedef struct _GdevI8255Class GdevI8255Class;
+
+struct _GdevI8255 {
+  GdevDevice parent_instance;
+};
+
+struct _GdevI8255Class {
+  GdevDeviceClass parent_class;
+};
+
+extern GType       gdev_i8255_get_type (void);
+extern GdevI8255 *gdev_i8255_new      (void);
+
+G_END_DECLS
+
+#endif /* __GDEV_I8255_H__ */
---
+++ src/dev/upd765.c
@@ -0,0 +1,54 @@
+/*
+ * upd765.c - Copyright (c) 2001, 2006 Olivier Poncet
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "upd765.h"
+
+G_DEFINE_TYPE(GdevUPD765, gdev_upd765, GDEV_TYPE_DEVICE)
+
+/**
+ * GdevUPD765::class_init()
+ *
+ * @param upd765_class specifies the GdevUPD765 class
+ */
+static void gdev_upd765_class_init(GdevUPD765Class *upd765_class)
+{
+}
+
+/**
+ * GdevUPD765::init()
+ *
+ * @param upd765 specifies the GdevUPD765 instance
+ */
+static void gdev_upd765_init(GdevUPD765 *upd765)
+{
+}
+
+/**
+ * GdevUPD765::new()
+ *
+ * @return the GdevUPD765 instance
+ */
+GdevUPD765 *gdev_upd765_new(void)
+{
+  return(g_object_new(GDEV_TYPE_UPD765, NULL));
+}
