b0d9037ef6356b073c455b20e21d406e291417be
Olivier Poncet <ponceto@free.fr>
2006-10-29 18:24:14 +0000

    Preliminary fdd support

---
+++ src/dev/fdd765.c
@@ -0,0 +1,81 @@
+/*
+ * fdd765.c - Copyright (c) 2001, 2006 Olivier Poncet
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fdd765.h"
+
+static void gdev_fdd765_reset(GdevFDD765 *fdd765);
+static void gdev_fdd765_clock(GdevFDD765 *fdd765);
+
+G_DEFINE_TYPE(GdevFDD765, gdev_fdd765, GDEV_TYPE_DEVICE)
+
+/**
+ * GdevFDD765::class_init()
+ *
+ * @param fdd765_class specifies the GdevFDD765 class
+ */
+static void gdev_fdd765_class_init(GdevFDD765Class *fdd765_class)
+{
+  GdevDeviceClass *device_class = GDEV_DEVICE_CLASS(fdd765_class);
+
+  device_class->reset = (GdevDeviceProc) gdev_fdd765_reset;
+  device_class->clock = (GdevDeviceProc) gdev_fdd765_clock;
+  fdd765_class->eject = NULL;
+}
+
+/**
+ * GdevFDD765::init()
+ *
+ * @param fdd765 specifies the GdevFDD765 instance
+ */
+static void gdev_fdd765_init(GdevFDD765 *fdd765)
+{
+  gdev_fdd765_reset(fdd765);
+}
+
+/**
+ * GdevFDD765::reset()
+ *
+ * @param fdd765 specifies the GdevFDD765 instance
+ */
+static void gdev_fdd765_reset(GdevFDD765 *fdd765)
+{
+}
+
+/**
+ * GdevFDD765::clock()
+ *
+ * @param fdd765 specifies the GdevFDD765 instance
+ */
+static void gdev_fdd765_clock(GdevFDD765 *fdd765)
+{
+}
+
+/**
+ * GdevFDD765::new()
+ *
+ * @return the GdevFDD765 instance
+ */
+GdevFDD765 *gdev_fdd765_new(void)
+{
+  return(g_object_new(GDEV_TYPE_FDD765, NULL));
+}
---
+++ src/dev/fdd765.h
@@ -0,0 +1,50 @@
+/*
+ * fdd765.h - Copyright (c) 2001, 2006 Olivier Poncet
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef __GDEV_FDD765_H__
+#define __GDEV_FDD765_H__
+
+#include <dev/device.h>
+#include <dev/upd765.h>
+
+G_BEGIN_DECLS
+
+#define GDEV_TYPE_FDD765            (gdev_fdd765_get_type())
+#define GDEV_FDD765(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj), GDEV_TYPE_FDD765, GdevFDD765))
+#define GDEV_FDD765_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass), GDEV_TYPE_FDD765, GdevFDD765Class))
+#define GDEV_IS_FDD765(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj), GDEV_TYPE_FDD765))
+#define GDEV_IS_FDD765_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), GDEV_TYPE_FDD765))
+#define GDEV_FDD765_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), GDEV_TYPE_FDD765, GdevFDD765Class))
+
+typedef struct _GdevFDD765      GdevFDD765;
+typedef struct _GdevFDD765Class GdevFDD765Class;
+
+struct _GdevFDD765 {
+  GdevDevice device;
+};
+
+struct _GdevFDD765Class {
+  GdevDeviceClass parent_class;
+  void (*eject)(GdevFDD765 *fdd765);
+};
+
+extern GType       gdev_fdd765_get_type (void);
+extern GdevFDD765 *gdev_fdd765_new      (void);
+
+G_END_DECLS
+
+#endif /* __GDEV_FDD765_H__ */
