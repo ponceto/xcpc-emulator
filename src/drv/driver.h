e78152f8fb68a2807bd7c69b23c8b00f247d8c36
Olivier Poncet <ponceto@free.fr>
2011-02-19 16:09:19 +0000

    Copyright modification for 2011

--- src/drv/driver.h
+++ src/drv/driver.h
@@ -1,5 +1,5 @@
 /*
- * driver.h - Copyright (c) 2001, 2006, 2007, 2008, 2009 Olivier Poncet
+ * driver.h - Copyright (c) 2001, 2006, 2007, 2008, 2009, 2010, 2011 Olivier Poncet
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
